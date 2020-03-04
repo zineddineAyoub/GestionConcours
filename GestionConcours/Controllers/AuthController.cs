126817911c6c60cf94db4633324e120e58ab17c8
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2020-03-04 22:08:46 +0100

    Login And Register Template Added

---
+++ GestionConcours/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionConcours.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
