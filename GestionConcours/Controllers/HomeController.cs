b4990af10b4ebf8fe6c0bf1389acd18c8e78789a
ZINEB KHALIS <khalis.zineb0@gmail.com>
2020-03-17 23:13:38 +0100

    Modification des informations du Baccalauréat

--- GestionConcours/Controllers/HomeController.cs
+++ GestionConcours/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GestionConcours.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +10,17 @@ namespace GestionConcours.Controllers
 {
     public class HomeController : Controller
     {
+        private GestionConcourDbContext db = new GestionConcourDbContext();
+        private Candidat candidat;
+
         public ActionResult Index()
         {
             if (Session["cne"] == null)
             {
                 return RedirectToAction("Login", "Auth");
             }
+
+            candidat = db.Candidats.Find(Session["cne"]);
             return View();
         }
 
@@ -34,8 +41,48 @@ namespace GestionConcours.Controllers
 
         public ActionResult ModifierBac()
         {
-            return View();
+            if (Session["cne"] == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            Baccalaureat bac = db.Baccalaureats.Find(Session["cne"]);
+
+            List<SelectListItem> listTypeBac = new List<SelectListItem>
+            {
+                new SelectListItem{Text="SMA", Value="SMA"},
+                new SelectListItem{Text="SMB", Value="SMB"},
+                new SelectListItem{Text="SVT", Value="SVT"},
+                new SelectListItem{Text="PC", Value="PC"}
+            };
+            List<SelectListItem> listMention = new List<SelectListItem>
+            {
+                new SelectListItem{Text="Très Bien", Value="Très Bien"},
+                new SelectListItem{Text="Bien", Value="Bien"},
+                new SelectListItem{Text="Assez Bien", Value="Assez Bien"},
+                new SelectListItem{Text="Passable", Value="Passable"}
+            };
+
+            ViewBag.typeBac = listTypeBac;
+            ViewBag.mention = listMention;
+
+            return View(bac);
         }
+
+        [HttpPost]
+        public ActionResult ModifierBac([Bind(Include = "Cne,TypeBac,DateObtentionBac,NoteBac,MentionBac")]Baccalaureat bac)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bac).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bac);
+
+
+        }
+
 
         public ActionResult ModifierFiliere()
         {
@@ -61,6 +108,8 @@ namespace GestionConcours.Controllers
         {
             return View();
         }
+
+
 
     }
 }
